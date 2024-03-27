9f3044c3284764887ddbfcc523297fdfc2b4bdc5
Aaranya <aaranyaalexander@gmail.com>
2024-03-26 23:55:54 -0400

    trying these for deployment

--- testing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("demohistory.csv")
-
-df.set_index("Overall Rating")
-num = len(df)
-usr = ["user_" + str(num +1)]
-df.loc[num] = ["Good", usr[0], 0, 0, 0, 0, 0, 0, 0]
-print(df)
